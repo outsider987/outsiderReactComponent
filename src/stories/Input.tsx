d7ece4c817d9e61e1a76d21a3ba24417002f4118
outsider987 <t7902195204@gmail.com>
2022-07-09 17:24:15 +0800

    fix:add backgrounf option for control

--- src/stories/Input.tsx
+++ src/stories/Input.tsx
@@ -9,12 +9,14 @@ interface Props {
   type: string;
   setInput: (value: any) => void;
   autoFocus?: boolean;
+  backgroundColor?:string
 }
 const Input: React.FC<Props> = (props: Props) => {
   return (
     <div className={`flex flex-col ` + props.className}>
       {props.label != undefined && <span className="flex">{props.label}</span>}
       <input
+        style={{backgroundColor: props.backgroundColor}}
         className={`test w-auto focus:outline-none focus:border-[#FF9B33] 
             focus:opacity-100 tracking-[0.25px] placeholder:opacity-30
           bg-black border-[3px] border-solid leading-[150%]
