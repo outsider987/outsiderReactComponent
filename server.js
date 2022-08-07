68886c50e673e9f05cada636d27ce8333911e5fc
outsider987 <t7902195204@gmail.com>
2022-08-07 22:48:08 +0800

    fix: refine path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'storybook-static')));
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
