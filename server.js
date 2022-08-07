4b1f73b2cf0b27c0db15c9b09929acf15c80a4b1
outsider987 <t7902195204@gmail.com>
2022-08-07 22:54:53 +0800

    fix:refine serve script

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 
-
+const app = express();
 app.use(express.static(path.join(__dirname, 'storybook-static')));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8079);
 
 var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+  console.log(`listening on port http://localhost:${server.address().port}`);
 });
